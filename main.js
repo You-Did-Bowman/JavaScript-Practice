8dc75c46db9f902e51dfad2d7e76a2bb53bbe04a
Judith Bohmann <judith.bohmann@dci-student.org>
2024-11-06 14:54:22 +0100

    solve cat

--- main.js
+++ main.js
@@ -1,263 +1,47 @@
-/* ***** BETTER TYPE OF *****
-
-Create a function called betterTypeOf that accepts a single parameter called value and returns the type of that value.
-
-For example:
-betterTypeOf(1); // 'number'
-betterTypeOf("hello"); // 'string'
-betterTypeOf(true); // 'boolean'
-betterTypeOf(""); // 'string'
-betterTypeOf([1, 2, 3]); // 'array'
-betterTypeOf(["a", "b", "c"]); // 'array'
-const person = {
-    fullName: "Sandy Smith",
-    age: 28
-};
-betterTypeOf(person); // "object"
-
-Pay particular attention at the cases where the value is an array. 
+/* 
+--------------------------- The Cat Object ---------------------------
+Write yourself a virtual cat - animals with a CLI are so much nicer than ones with fur.
+Create an object that represents a cat. It should have properties for tiredness, hunger, lonliness and happiness
+Next, write methods that increase and decrease those properties (there's an example in the slides). Call them something that actually represents what would increase or decrease these things, like "feed", "sleep", or "pet".
+Last, write a method that prints out the cat's status in each area.
+Bonus: Make the functions take arguments that increase or decrease arbitrary amounts
+Bonus: Make the functions as arbitrary as cats are - sometimes make it so the cat doesn't want to be petted.
 */
-
-function betterTypeOf(value) {
-  if (Array.isArray(value)) {
-    console.log("array");
-  } else {
-    console.log(typeof value);
-  }
+  
+const cat = {
+    tiredness: 3,
+    hunger: 0,
+    lonliness: 7,
+    happiness: 10,
+    pet: function(num){
+        this.happiness += num;
+        this.lonliness += Math.floor(num/2);
+    },
+    sleep: function(hours){
+        this.tiredness -= Math.floor(hours/3);
+        this.happiness += Math.floor(hours/4);
+    },
+    feed: function(amount){
+        this.hunger += amount;
+        this.happiness += Math.floor(amount/2);
+    },
+    status: function(){
+       for (let elem in cat) {
+        if (typeof cat[elem] !== "function") {
+            console.log(`${elem}: ${cat[elem]}`);
+        }
+       }
+    }
 }
 
-betterTypeOf(1); // 'number'
-betterTypeOf("hello"); // 'string'
-betterTypeOf(true); // 'boolean'
-betterTypeOf(""); // 'string'
-betterTypeOf([1, 2, 3]); // 'array'
-betterTypeOf(["a", "b", "c"]); // 'array'
-const person = {
-  fullName: "Sandy Smith",
-  age: 28,
-};
-betterTypeOf(person); // "object"
+cat.status();
 
-/* ***** OBJECT: CREATE, ACCESS, UPDATE *****
+console.log(" ");
 
-This exercise is meant to let you properly understand and practice basic operations we perform with objects, tipically:
-- creating objects
-- accessing, reading, their properties
-- updating, modifying, the values of these properties
+cat.pet(5);
+cat.feed(10);
+cat.sleep(6);
 
------------------------------- Objects: Task 1 ------------------------------
+console.log(" ");
 
-Given the object below, change the value of the read property from false to true. Then, output the whole object to the console.
-const book = {
-    title: "A Smarter Way to Learn JavaScript",
-    author: "Mark Myers",
-    price: 19.95,
-    read: false
-};
-
-Expected output:
-{
-    title: "A Smarter Way to Learn JavaScript",
-    author: "Mark Myers",
-    price: 19.95,
-    read: true
-}
-*/
-const book = {
-  title: "A Smarter Way to Learn JavaScript",
-  author: "Mark Myers",
-  price: 19.95,
-  read: false,
-};
-
-book.read = true;
-
-console.log(book);
-
-/* 
------------------------------- Objects: Task 2 ------------------------------
-Given the object below, increase the age property by 1. Then, output the whole object to the console.
-const person = {
-    firstName: "Martha",
-    lastName: "Martin",
-    age: 27
-};
-
-Expected output:
-{
-    firstName: "Martha",
-    lastName: "Martin",
-    age: 28
-}; 
-*/
-const person1 = {
-  firstName: "Martha",
-  lastName: "Martin",
-  age: 27,
-};
-
-person1.age++;
-console.log(person1);
-
-/* 
------------------------------- Objects: Task 3 ------------------------------
-Given the object below, modify the value of the property discountedPrice so that it's half of the price property.
-Then, output a message to the console as shown below.
-const product = {
-    type: "hat",
-    color: "green",
-    brand: "Dolphin",
-    price: 22,
-    discountedPrice: 22,
-};
-IMPORTANT: when modifying discountedPrice, make sure to use the property price to calculate the new value, rather than typing 22 directly.
-
-Expected output:
-SALE: the green Dolphin hat costs now only 11€ instead of 22€! 
-*/
-const product = {
-  type: "hat",
-  color: "green",
-  brand: "Dolphin",
-  price: 22,
-  discountedPrice: 22,
-};
-product.discountedPrice = product.price / 2;
-
-console.log(
-  `SALE: the ${product.color} ${product.brand} ${product.type} costs now only ${product.price} instead of ${product.discountedPrice}!`
-);
-
-/* 
------------------------------- Objects: Task 4 ------------------------------
-Given the object below, add a new property to it called fullName.
-The property should combine the values from firstName and lastName, separated by a space.
-const person = {
-    firstName: "Jim",
-    lastName: "Jameson",
-    age: 37
-};
-
-Expected output:
-{
-    firstName: "Jim",
-    lastName: "Jameson",
-    age: 37,
-    fullName: "Jim Jameson"
-};
-*/
-const person2 = {
-  firstName: "Jim",
-  lastName: "Jameson",
-  age: 37,
-};
-
-person2["fullName"] = person2.firstName + " " + person2.lastName;
-
-/* 
------------------------------- Objects: Task 5 ------------------------------
-Given the object below, modify the value of the property city so that the first letter is correctly capitalised.
-const person = {
-    firstName: "Sylvia",
-    lastName: "Sun",
-    city: "london"
-};
-IMPORTANT: try to use and modify the current value of city rather than manually typing London.
-
-Expected output:
-{
-    firstName: "Sylvia",
-    lastName: "Sun",
-    city: "London"
-};
-*/
-const person3 = {
-  firstName: "Sylvia",
-  lastName: "Sun",
-  city: "london",
-};
-person3.city = person3.city[0].toUpperCase() + person3.city.slice(1);
-
-console.clear();
-
-/* 
-------------------------------DEV.TO: REAL WORLD OBJECTS EXERCISES ------------------------------- 
-***** Exercise 1: Creating Objects *****
-*/
-
-// Using Object Literals
-const person4 = {
-  name: "John",
-  age: 30,
-  profession: "Developer",
-};
-
-// Using Constructor Function
-function Person(name, age, profession) {
-  this.name = name;
-  this.age = age;
-  this.profession = profession;
-}
-const person5 = new Person("Jane", 25, "Designer");
-
-// Using Object.create() Method
-const person6 = Object.create(null);
-person3.name = "Mike";
-person3.age = 35;
-person3.profession = "Engineer";
-
-// ***** Exercise 2: Accessing Object Properties *****
-console.log(person1.name); // Output: John
-console.log(person2["age"]); // Output: 25
-
-// ***** Exercise 3: Modifying Object Properties *****
-person1.age = 32;
-person2["profession"] = "Manager";
-
-// ***** Exercise 4: Adding New Properties *****
-person1.location = "New York";
-person2["salary"] = 60000;
-
-// ***** Exercise 5: Iterating Through Object Properties *****
-  // Using for...in loop
-  for (let key in person1) {
-    console.log(`${key}: ${person1[key]}`);
-  };
-  
-  // Using Object.keys() method
-  Object.keys(person2).forEach(key => {
-    console.log(`${key}: ${person2[key]}`);
-  });
-  
-// ***** Exercise 6: Checking Property Existence *****
-console.log("name" in person1); // Output: true
-console.log(person3.hasOwnProperty("salary")); // Output: false
-  
-// ***** Exercise 7: Deleting Properties *****
-delete person1.location;
-delete person2.salary;
-  
-// ***** Exercise 8: Object Methods *****
-person1.greet = function() {
-    console.log(`Hello, my name is ${this.name}.`);
-};
-person2.introduce = () => {
-    console.log(`I'm ${person2.name}, a ${person2.age}-year-old ${person2.profession}.`);
-};
-  
-// ***** Exercise 9: Object Composition *****
-const address = {
-    city: "San Francisco",
-    country: "USA"
-};
-  
-const personWithAddress = { ...person1, ...address };
-console.log(personWithAddress);
-  
-// ***** Exercise 10: Object Serialization and Deserialization *****
-const jsonPerson = JSON.stringify(person1);
-console.log(jsonPerson);
-  
-const parsedPerson = JSON.parse(jsonPerson);
-console.log(parsedPerson);
-  
+cat.status();
