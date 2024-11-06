7efe2d00c799921d26c41e0171afba1007c1bb95
Judith Bohmann <judith.bohmann@dci-student.org>
2024-11-06 11:57:34 +0100

    solve tas2 of ex 'Objects'

--- main.js
+++ main.js
@@ -76,3 +76,27 @@ console.log(book);
 
 console.clear ();
 
+/* 
+------------------------------ Task 2 ------------------------------
+Given the object below, increase the age property by 1. Then, output the whole object to the console.
+const person = {
+    firstName: "Martha",
+    lastName: "Martin",
+    age: 27
+};
+Expected output:
+{
+    firstName: "Martha",
+    lastName: "Martin",
+    age: 28
+}; 
+*/
+const person1 = {
+    firstName: "Martha",
+    lastName: "Martin",
+    age: 27
+};
+
+person1.age++;
+console.log(person1);
+
