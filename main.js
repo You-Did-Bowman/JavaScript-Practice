f99c2501b0cd1ab4003beee510ce9f19d696843c
Judith Bohmann <judith.bohmann@dci-student.org>
2024-11-06 11:39:19 +0100

    change comments and add second task

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-/* 
----------------------------- TASK 1 ----------------------------
-** BETTER TYPE OF **
+/* ***** BETTER TYPE OF *****
 
 Create a function called betterTypeOf that accepts a single parameter called value and returns the type of that value.
 
@@ -40,3 +38,28 @@ const person = {
     age: 28
 };
 betterTypeOf(person); // "object"
+
+/* ***** OBJECT: CREATE, ACCESS, UPDATE *****
+
+This exercise is meant to let you properly understand and practice basic operations we perform with objects, tipically:
+creating objects
+accessing, reading, their properties
+updating, modifying, the values of these properties
+Task 1
+Given the object below, change the value of the read property from false to true. Then, output the whole object to the console.
+const book = {
+    title: "A Smarter Way to Learn JavaScript",
+    author: "Mark Myers",
+    price: 19.95,
+    read: false
+};
+Expected output:
+{
+    title: "A Smarter Way to Learn JavaScript",
+    author: "Mark Myers",
+    price: 19.95,
+    read: true
+}
+
+
+*/
