4a069573fe178727624de58bdf60d14028c06e0d
Judith Bohmann <judith.bohmann@dci-student.org>
2024-11-07 21:09:20 +0100

    solve nite owl from codedex

--- index_2024-11-7.js
+++ index_2024-11-7.js
@@ -344,3 +344,26 @@ for (let i = 1; i < 9; i++) {
   secondFib = nextFib;
   console.log(nextFib);
 }
+
+console.clear();
+
+/* 
+------------------------------ NITE OWL ------------------------------
+People tend to be either an early bird or a night owl. There are several factors that go into this, such as genetics, age, lifestyle, and more.
+
+Let's use conditionals to see which one you are!
+
+Start by creating a wakeUp variable and set it equal to the time you wake up, between 0 and 23.
+
+Test for early bird vs. night owl with an if/else statement:
+- wakeUp is < 8, log "Early bird gets the worm! 🌅"
+- Otherwise, log "But who wants the worm? 🌃"
+*/
+
+let wakeUp = 7;
+
+if (wakeUp < 8) {
+  console.log("Early bird gets the worm! 🌅");
+} else {
+  console.log("But who wants the worm? 🌃");
+}
