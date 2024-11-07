66d6ae8ca59857dec4638577503367607529efcf
Judith Bohmann <judith.bohmann@dci-student.org>
2024-11-07 11:16:55 +0100

    solve task 1 - 3

--- index_2024-11-7.js
+++ index_2024-11-7.js
@@ -1,68 +1,102 @@
 /* -------------------------- Task 1 --------------------------
-Given an array of products called cart and a product object hatProduct:
-
-const cart = [
-{
-type: "t-shirt",
-price: 19,
-color: "green",
-},
-{
-type: "jeans",
-price: 27,
-color: "blue",
-}
-];
-
-const hatProduct = {
-type: "hat",
-price: 15,
-color: "pink",
-};
+Given an array of products called cart and a product object hatProduct.
 
 Create a function called addProduct that takes the cart array and the hat product in input.
 The function should add the new product at the end of the array and return the updated cart.
 Make sure that the original cart is not modified. So, after calling the function, cart still has only 2 products and the function returns a new array with 3 products. */
 
+const cart = [
+  {
+    type: "t-shirt",
+    price: 19,
+    color: "green",
+  },
+  {
+    type: "jeans",
+    price: 27,
+    color: "blue",
+  },
+];
+
+const hatProduct = {
+  type: "hat",
+  price: 15,
+  color: "pink",
+};
+
+const newCart = [...cart]
+
+const addProduct = (product) => {
+    newCart.push(product);
+    return newCart;
+};
+ let addNewProduct = addProduct(hatProduct);
+ console.log(addNewProduct); // [{ type: 't-shirt', price: 19, color: 'green' }, { type: 'jeans', price: 27, color: 'blue' }, { type: 'hat', price: 15, color: 'pink' }]
+ console.log(cart); //[{ type: 't-shirt', price: 19, color: 'green' }, { type: 'jeans', price: 27, color: 'blue' }]
+ 
 
 
 /* -------------------------- Task 2 --------------------------
-Given an array tasks containing a series of objects:
-const tasks = [
-{
-task: "clean apartment",
-priority: 5
-},
-{
-task: "learn objects",
-priority: 2
-},
-{
-task: "practice JavaScript",
-priority: 1
-}
-]
+Given an array tasks containing a series of objects.
 
 Create a function called finishTask that:
-
-    takes in input an array of tasks
-    removes the last item from the array
-    returns the updated array
+- takes in input an array of tasks
+- removes the last item from the array
+- returns the updated array
 
 The original task array must not be modified. */
 
+const tasks = [
+  {
+    task: "clean apartment",
+    priority: 5,
+  },
+  {
+    task: "learn objects",
+    priority: 2,
+  },
+  {
+    task: "practice JavaScript",
+    priority: 1,
+  },
+];
+
+const newTasks = [...tasks];
+
+const finishTask = (array) => {
+    newTasks.pop();
+    return newTasks
+};
+let newTaksList = finishTask(tasks);
+console.log(newTaksList); //[{ task: 'clean apartment', priority: 5 }, { task: 'learn objects', priority: 2 }]
+console.log(tasks); // [{ task: 'clean apartment', priority: 5 }, { task: 'learn objects', priority: 2 }, { task: 'practice JavaScript', priority: 1 }]
+
+
 /* -------------------------- Task 3 --------------------------
-Given an object person:
-const person = {
-firstName: "James",
-lastName: "Ensor",
-age: 13
-}
+Given an object person.
 
 Create a function called addFullName that takes in input an object and adds a new property to it called fullName.
 The function returns the modified version of the object. The property should combine together firstName and lastName.
 The original person object must not be changed. */
 
+const person = {
+  firstName: "James",
+  lastName: "Ensor",
+  age: 13,
+};
+
+const newPerson = { ...person };
+
+const addFullName = (obj) => {
+    newPerson["fullName"] = obj.firstName + " " + obj.lastName;
+    return newPerson;
+}
+
+let updatedPerson = addFullName(person);
+console.log(updatedPerson); // {firstName: 'James', lastName: 'Ensor', age: 13, fullName: 'James Ensor'}
+console.log(person); // { firstName: 'James', lastName: 'Ensor', age: 13 }
+
+console.clear();
 
 /* -------------------------- Task 4 --------------------------
 Given the array of product objects:
