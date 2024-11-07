bcc5d57e185fe99e597985c779d41d62228b1535
Judith Bohmann <judith.bohmann@dci-student.org>
2024-11-07 20:31:24 +0100

    solve dice double from codedex

--- index_2024-11-7.js
+++ index_2024-11-7.js
@@ -166,8 +166,6 @@ const products = [
   },
 ];
 
-// this should be in the function
-
 const applyDiscount = (arr) => {
   const newProducts = structuredClone(arr);
   for (let i = 0; i < arr.length; i++) {
@@ -252,7 +250,6 @@ while (count != 10) {
 
 console.log(`The number ${targetNumber} was found 10 times`);
 
-console.clear();
 
 /* ------------------------------ ODD CUBES ------------------------------
 When you multiply any number three times, you're cubing it (e.g., 3³ = 3 x 3 x 3 = 27).
@@ -283,4 +280,42 @@ for (let i = 1; i <= limit; i++) {
 
 console.log(total);
 
+console.clear();
+
+/* 
+------------------------------ DICE DOUBLE ------------------------------
+If you roll two dice, the chance that both of them come up with the same value is slightly less than 17%. 🎲 🎲
+
+Let's simulate this with loops!
+
+Start with two variables, die1 and die2, each set with a random value between 1 and 6.
+
+Let's track the dice rolls by initializing a numberOfRolls at 0.
+
+Now, let's use a while loop that runs as long as our die1 is not equal to die2. When they're not equal, increment numberOfRolls and reset the die1 and die2 to a new random number between 1 and 6.
+
+When you roll double values, the loop should stop and the output should look like this:
+
+Roll 1: 2
+Roll 2: 5
+Roll 1: 1
+Roll 2: 1
+Number of rolls: 1
+
+Note: Make sure to use console.log() to match the output shown above.
+*/
+let die1 = Math.ceil(Math.random() * 6);
+let die2 = Math.ceil(Math.random() * 6);
+let numberOfRolls = 0;
+
+while (die1 != die2) {
+    die1 = Math.ceil(Math.random() * 6);
+    die2 = Math.ceil(Math.random() * 6);
+    numberOfRolls++;
+    console.log(`
+      Roll1: ${die1}
+      Roll2: ${die2}`);
+}
+console.log(`
+  Number of rolls: ${numberOfRolls}`);
 
