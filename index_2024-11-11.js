f2304d2efc591a1e2cf3f001380ea5a40825863d
Judith Bohmann <judith.bohmann@dci-student.org>
2024-11-11 14:36:29 +0100

    solve ex 1

--- index_2024-11-11.js
+++ index_2024-11-11.js
@@ -1,24 +1,23 @@
-Exercise 1
+/*
+---------------------------- Exercise 1 ----------------------------
 Rewrite the code below to use array destructuring instead of assigning each value to a variable.
 */
-{
-  console.log("EXERCISE 1");
 
-  let item = ["Egg", 0.25, 12];
+console.log("EXERCISE 1");
 
-  let name = item[0];
-  let price = item[1];
-  let quantity = item[2];
+let item = ["Egg", 0.25, 12];
 
-  console.log(`Item: ${name}, Quantity: ${quantity}, Price: ${price}`);
-  console.log();
-}
+let [name, price, quantity] = item;
+
+console.log(`Item: ${name}, Quantity: ${quantity}, Price: ${price}`);
+
+console.clear();
 
 /**
 Exercise 2
 Rewrite the code below to assign each number to the right variable.
 */
-{
+/* {
   console.log("EXERCISE 2");
 
   let numbers = [3, 5, 4, 2, 6, 1];
@@ -27,7 +26,7 @@ Rewrite the code below to assign each number to the right variable.
 
   console.log(`One: ${one}, Two: ${two}, Three: ${three}, Four: ${four}, Five: ${five}, Six: ${six}`);
   console.log();
-}
+} */
 
 /**
 Exercise 3
@@ -37,7 +36,7 @@ Write the destructuring assignment that reads:
 - 'years' property into the variable 'age'.
 - 'isAdmin' property into the variable 'isAdmin' (false, if no such property)
 */
-{
+/* {
   console.log("EXERCISE 3");
 
   let user = { name: "John", years: 30 };
@@ -49,13 +48,13 @@ Write the destructuring assignment that reads:
   console.log(age); // 30
   console.log(isAdmin); // false
   console.log();
-}
+} */
 
 /**
 Exercise 4
 Rewrite the code below to use array destructuring instead of assigning each value to a variable.
 */
-{
+/* {
   console.log("EXERCISE 4");
 
   let person = [12, "Chris", "Owen"];
@@ -66,7 +65,7 @@ Rewrite the code below to use array destructuring instead of assigning each valu
 
   console.log(`Person - Age: ${age}, Name: ${firstName} ${lastName}`);
   console.log();
-}
+} */
 
 /** 
 Exercise 5
@@ -74,7 +73,7 @@ Rewrite the code below to use array destructuring instead of assigning each valu
 Make sure not to have unused variables.
 Hint: https://untangled.io/in-depth-es6-destructuring-with-assembled-avengers
 */
-{
+/* {
   console.log("EXERCISE 5");
 
   let person = ["Chris", 12, "Owen"];
@@ -84,14 +83,14 @@ Hint: https://untangled.io/in-depth-es6-destructuring-with-assembled-avengers
 
   console.log(`Name: ${firstName} ${lastName}`);
   console.log();
-}
+} */
 
 /** 
 Exercise 6
 Using Array Destructuring get the last name from the array. 
 Hint: https://untangled.io/in-depth-es6-destructuring-with-assembled-avengers
 */
-{
+/* {
   console.log("EXERCISE 6");
 
   const students = ['Christina', 'Jon', 'Alexandare'];
@@ -101,14 +100,14 @@ Hint: https://untangled.io/in-depth-es6-destructuring-with-assembled-avengers
 
   console.log(lastName);
   console.log();
-}
+} */
 
 /**
 Exercise 7
 Using Array Destructuring get all of the names from this Nested Array
 Hint: https://untangled.io/in-depth-es6-destructuring-with-assembled-avengers
 */
-{
+/* {
   console.log("EXERCISE 7");
 
   const moreStudents = [
@@ -122,4 +121,4 @@ Hint: https://untangled.io/in-depth-es6-destructuring-with-assembled-avengers
 
   console.log(student1, student2, student3, student4, student5);
   console.log();
-}
+} */
