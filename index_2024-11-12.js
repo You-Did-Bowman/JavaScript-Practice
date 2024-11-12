dfcd5cb2e9b552e0ce9141bee809ac193b3de014
Judith Bohmann <judith.bohmann@dci-student.org>
2024-11-12 21:36:08 +0100

    solve task 2

--- index_2024-11-12.js
+++ index_2024-11-12.js
@@ -41,4 +41,46 @@ const showNameAge = (el) => {
 };
 people.forEach(showNameAge);
 
+console.log();
 
+
+/* ----------------------- Task 2 -----------------------
+
+Use forEach to log only the even numbers to the console
+*/
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log("****** TASK 2 ******");
+
+//Normal function ------------
+console.log("Normal function:");
+
+numbers.forEach(function(el) {
+    if (el % 2 === 0) {
+        return console.log(el);
+    }
+});
+
+console.log();
+
+
+//Arrow function ------------
+console.log("Arrow function:");
+
+numbers.forEach((el) => {
+    if (el % 2 === 0) {
+        return console.log(el);
+    }
+});
+
+console.log();
+
+
+// Pass existing function ------------
+console.log("Pass existing function:");
+
+const findEven = (el) => {
+    if (el % 2 === 0) {
+        return console.log(el);
+    }
+};
+numbers.forEach(findEven);
