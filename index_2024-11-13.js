f0e7dae0970f981c288eebab366197653426b9b3
Judith Bohmann <judith.bohmann@dci-student.org>
2024-11-13 21:09:12 +0100

    solve task 2

--- index_2024-11-13.js
+++ index_2024-11-13.js
@@ -6,6 +6,7 @@ const numbers = [2, 3, 4];
 
 Use the reduce() method to calculate the product of all numbers in the array.
 */
+console.log("****** TASK 1 ******");
 
 const numbers = [2, 3, 4];
 
@@ -15,4 +16,29 @@ const product = numbers.reduce((acc, el) => {
 
 console.log(product); // 24
 
+console.log();
 
+
+/* --------------------------- TASK 2 ---------------------------
+
+Find the longest word in an array of words.
+
+const words = ["apple", "banana", "cherry", "watermelon", "grape"];
+
+Use the reduce() method to find the longest word in the array. The final result should be the longest word as a string.
+
+*/
+console.log("****** TASK 2 ******");
+
+const words = ["apple", "banana", "cherry", "watermelon", "grape"];
+
+const longestWord = words.reduce((acc, el) => {
+    // compare the length of el with the length of acc -> if el is longer it will be the new acc
+    if (el.length > acc.length) {
+        acc = el;
+    };
+    // return the longest acc
+    return acc
+}, "");
+
+console.log(longestWord); // watermelon
